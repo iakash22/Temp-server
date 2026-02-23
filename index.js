4ea8a0ade1d7b5e0fbee6897dceb629cf5acc6b8
Akash kumar <94979388+iakash22@users.noreply.github.com>
2026-02-23 19:30:55 +0530

    New Ip checker create v2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", true);
 
 app.get('/', (req, res) => {
     res.send(`<h1>Server Running Perfect.</h1>`)
@@ -20,6 +23,19 @@ app.get('/get-ip', (req, res) => {
 
     res.send(`Your IP is: ${ip}`);
 })
+
+app.get("v2/get-ip", (req, res) => {
+    let ip =
+        req.headers["x-forwarded-for"]?.split(",")[0] ||
+        req.socket.remoteAddress;
+
+    // Convert IPv6-mapped IPv4 to normal IPv4
+    if (ip && ip.startsWith("::ffff:")) {
+        ip = ip.substring(7);
+    }
+
+    res.json({ ip });
+});
 
 
 app.listen(PORT, () => {
